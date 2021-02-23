0434abea3e5328d4dae6c5d764305c445cf85349
Chris Giroux <chris.giroux86@gmail.com>
2021-02-23 17:15:10 -0500

    staging for new PR

--- newfile.js
+++ newfile.js
@@ -5,7 +5,7 @@ console.log('this is the new file from main!!!');
 console.log('this is the new file!!!!');
 
 // (TODO): This can be removed once we're sure everything is working as expected
-console.log('Hello from the outside!');
+console.log('Hello from the outside!!!');
 // function mergeEmptyPacketWithLastRevision(
 //   review, firstPacket: Packet, commitsIndex: Dict<GithubCommit>) {
 //   if (_.isEmpty(review.revisions) || !firstPacket) return false;
