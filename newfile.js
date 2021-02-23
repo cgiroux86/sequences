e9c4342be132b4c2ce58349af127a56bf34b3624
Chris Giroux <chris.giroux86@gmail.com>
2021-02-23 15:41:46 -0500

    old new file changed

--- newfile.js
+++ newfile.js
@@ -1,6 +1,6 @@
 
 // (TODO): This can be removed once we're sure everything is working as expected
-console.log('old test file name');
+console.log('old test file name!');
 // function mergeEmptyPacketWithLastRevision(
 //   review, firstPacket: Packet, commitsIndex: Dict<GithubCommit>) {
 //   if (_.isEmpty(review.revisions) || !firstPacket) return false;
